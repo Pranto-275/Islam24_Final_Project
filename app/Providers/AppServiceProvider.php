3d7fa46bbd7c529da5caef2c92d50869bdcb8792
Nazmul Huda <55596925+nazmul2858@users.noreply.github.com>
2021-06-26 15:45:06 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,17 +2,15 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Models\Backend\ProductInfo\Category;
+use App\Models\Backend\ProductInfo\Product;
 use App\Models\Backend\ProductInfo\SubCategory;
 use App\Models\Backend\ProductInfo\SubSubCategory;
-use App\Models\Backend\ProductInfo\Product;
+use App\Models\Backend\Setting\CompanyInfo as SettingCompanyInfo;
 use App\Models\Setting\Slider;
-use App\Models\Setting\CompanyInfo;
 use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\View;
-
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,7 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 
     /**
@@ -34,14 +31,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //Categories
-        View::composer('*', function($view){
+        View::composer('*', function ($view) {
             $view->with('categories', Category::orderBy('id', 'desc')->get());
             $view->with('topCategories', Category::whereTopShow(1)->take(5)->get());
             $view->with('subCategories', SubCategory::orderBy('id', 'desc')->get());
             $view->with('subSubCategories', SubSubCategory::orderBy('id', 'desc')->get());
             $view->with('products', Product::orderBy('id', 'desc')->get());
             $view->with('sliderImages', Slider::orderBy('position')->take(4)->get());
-//            $view->with('companyInfo', CompanyInfo::whereUserId(Auth::id())->first());
+            $view->with('companyInfo', SettingCompanyInfo::whereUserId(Auth::id())->first());
         });
     }
 }
