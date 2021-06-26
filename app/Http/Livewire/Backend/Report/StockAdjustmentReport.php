4e6f1a6f83a90cff8c16cf08411b2732bb384430
masumbillah1122 <68697904+masumbillah1122@users.noreply.github.com>
2021-06-26 17:49:26 +0600

    done

--- app/Http/Livewire/Backend/Report/StockAdjustmentReport.php
+++ app/Http/Livewire/Backend/Report/StockAdjustmentReport.php
@@ -5,6 +5,10 @@ namespace App\Http\Livewire\Backend\Report;
 
 use App\Models\Backend\Inventory\StockAdjustment;
 use Carbon\Carbon;
+use App\Models\Backend\ContactInfo\Contact;
+use App\Models\Backend\Setting\Branch;
+use App\Models\Backend\Setting\Warehouse;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class StockAdjustmentReport extends Component
@@ -12,9 +16,11 @@ class StockAdjustmentReport extends Component
     public $to_date = Null;
     public $from_date = Null;
     public $second=NULL;
+    public $type;
+    public $contact_id;
+    public $branch_id;
+
    
-    public $Query;
-    public $StockAdjustmentReportId=NULL;
     public function dateFilter($model){
         return $model->where('date', '>=', Carbon::parse($this->from_date)->format('Y-m-d'))->where('date', '<=', Carbon::parse($this->to_date)->format('Y-m-d'));
     }
@@ -26,7 +32,14 @@ class StockAdjustmentReport extends Component
 
             'to_date' => 'required',
             'from_date' => 'required',
-            
+            'contact_id' => 'required',
+            'branch_id' => 'required',
+
+            // 'type' => 'required',
+            // 'from_branch_id' => 'required',
+            // 'to_branch_id' => 'required',
+            // 'from_warehouse_id' => 'required',
+            // 'to_warehouse_id' => 'required',
         ]);
 
      
@@ -37,11 +50,21 @@ class StockAdjustmentReport extends Component
                $Query=new StockAdjustment();
             }
   
-            $Query->to_date=$this->to_date;
-            $Query->from_date=$this->from_date;
-           
+            $Query->date=$this->date;
+            $Query->date=$this->date1;
+            $Query->contact_id= $this->contact_id;
+            $Query->to_branch_id=$this->to_branch_id;
+
+            // $Query->type=$this->type;
+            // $Query->from_branch_id=$this->from_branch_id;
+            // $Query->from_warehouse_id=$this->from_warehouse_id;
+            // $Query->to_warehouse_id=$this->to_warehouse_id;
+            // $Query->note=$this->note;
             $Query->save();
            //    Stock
+           
+   
+
         $this->reset();
         $this->emit('success', [
             'text' => 'Stock Adjustment C/U Successfully',
@@ -50,10 +73,14 @@ class StockAdjustmentReport extends Component
 
     public function render()
     {
-        
 
         return view('livewire.backend.report.stock-adjustment-report',[
             'stocks' => StockAdjustment::get(),
+            'contacts' => Contact::get(),
+            'branches' => Branch::get(),
+
+            // 'dates' => StockAdjustment::get(),
+            // 'warehouses' => Warehouse::get(),
         ]);
     }
 }
