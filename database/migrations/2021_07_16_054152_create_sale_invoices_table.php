d903bafe27e84d3723a14c360cd228349c8e5ecd
Md. Maraj Hossain <marajmmc@gmail.com>
2021-07-16 12:07:55 +0600

    migration wish lists

--- database/migrations/2021_07_16_054152_create_sale_invoices_table.php
+++ database/migrations/2021_07_16_054152_create_sale_invoices_table.php
@@ -24,6 +24,7 @@ class CreateSaleInvoicesTable extends Migration
             $table->double('payable_amount')->nullable();
             $table->text('note')->nullable();
             $table->enum('invoice_channel', ['Web-Sale', 'Sale-Terminal'])->comment('Backend Sale or Online Sale');
+            $table->foreignId('coupon_code_id')->nullable();
             $table->foreignId('branch_id');
             $table->foreignId('created_by');
             $table->boolean('is_active')->nullable()->default(1);
