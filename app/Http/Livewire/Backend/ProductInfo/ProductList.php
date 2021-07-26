d85e022712dd2128840c8cf4f31311d94ede3608
Nazmul <nazmul2858@gmail.com>
2021-07-26 17:43:33 +0600

    Product List

--- app/Http/Livewire/Backend/ProductInfo/ProductList.php
+++ app/Http/Livewire/Backend/ProductInfo/ProductList.php
@@ -4,12 +4,14 @@ namespace App\Http\Livewire\Backend\ProductInfo;
 use App\Models\Backend\ProductInfo\Product;
 use App\Models\Backend\ProductInfo\ProductImage;
 use App\Models\Backend\ProductInfo\ProductProperties;
+use App\Models\Backend\ProductInfo\ProductInfo;
 use Livewire\Component;
 
 class ProductList extends Component
 {
     public function deleteProduct($id){
         Product::find($id)->delete();
+        ProductInfo::whereProductId($id)->delete();
         ProductImage::whereProductId($id)->delete();
         ProductProperties::whereProductId($id)->delete();
 
