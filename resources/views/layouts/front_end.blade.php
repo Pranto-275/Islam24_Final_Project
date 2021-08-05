bc0842cd58f4dcf389d69ec57119151abd484c00
Nazmul Huda <55596925+nazmul2858@users.noreply.github.com>
2021-08-06 00:56:47 +0600

    Update app name

--- resources/views/layouts/front_end.blade.php
+++ resources/views/layouts/front_end.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Paikari Electronics') }}</title>
+        <title>{{ config('app.name') }} </title>
         <meta name="keywords" content="html5 template, best html5 template, best html template, html5 basic template, multipurpose html5 template, multipurpose html template, creative html templates, creative html5 templates" />
         <meta name="description" content="SuperMarket is a powerful Multi-purpose HTML5 Template with clean and user friendly design. It is definite a great starter for any eCommerce web project." />
         <meta name="author" content="Magentech">
