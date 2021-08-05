4d8bf42d145ea418497bab20aed4af87c43cd194
Nazmul <nazmul2858@gmail.com>
2021-08-05 13:15:00 +0600

    Updated Footer

--- resources/views/frontend/mobile-responsive-footer.blade.php
+++ resources/views/frontend/mobile-responsive-footer.blade.php
@@ -7,12 +7,12 @@
                     <a href="{{url('/')}}"><img src="{{ URL::asset('venam/') }}/img/logo/logo_paikari_red.png" alt="Logo"></a>
                 </div>
             </div>
-            <div class="col-3 text-center">
+            <div class="col-3 py-3 text-center">
                 {{-- <i class="fas fa-shopping-cart mt-2" style="font-size: 30px;"></i> --}}
-                <li class="header-shop-cart"><a href="{{ route('check-out') }}"><i class="fas fa-shopping-cart mt-2" style="font-size: 30px;"></i><span class="cart-count">{{ $cardBadge['data']['number_of_product'] }}</span></a>
+                <li class="header-shop-cart"><a href="{{ route('check-out') }}"><i class="fas fa-shopping-cart" style="font-size: 30px;"></i><span class="cart-count">{{ $cardBadge['data']['number_of_product'] }}</span></a>
             </div>
-            <div class="col-3 text-center">
-                <i class="fab fa-facebook-messenger mt-2"  style="font-size: 30px;"></i>
+            <div class="col-3 py-3 text-center">
+                <i class="fab fa-facebook-messenger"  style="font-size: 30px;"></i>
             </div>
           </div>
         </div>
