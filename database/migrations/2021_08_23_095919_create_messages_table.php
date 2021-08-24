c610253f7345954f5f26b9e071f04a8ca38783c8
Nazmul Huda <55596925+nazmul2858@users.noreply.github.com>
2021-08-24 14:16:56 +0600

    Update 2021_08_23_095919_create_messages_table.php

--- database/migrations/2021_08_23_095919_create_messages_table.php
+++ database/migrations/2021_08_23_095919_create_messages_table.php
@@ -17,7 +17,7 @@ class CreateMessagesTable extends Migration
             $table->increments('id');
             $table->string('first_name', 100);
             $table->string('last_name', 100);
-            $table->string('email', 191);
+            $table->string('email', 191)->nullable();
             $table->string('phone', 100);
             $table->text('subject');
             $table->text('message');
