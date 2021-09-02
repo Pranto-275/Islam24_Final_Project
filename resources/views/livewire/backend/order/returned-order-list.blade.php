3ecc293055060bd70b98748cdffc232895fdcce8
Nazmul <59364699+iqbal17115@users.noreply.github.com>
2021-09-02 17:00:11 +0600

    Updated

--- resources/views/livewire/backend/order/returned-order-list.blade.php
+++ resources/views/livewire/backend/order/returned-order-list.blade.php
@@ -39,8 +39,7 @@
                             </div>
                             <div class="table-responsive">
                                 <div class="table-responsive">
-                                    <table class="table table-centered mb-0 table-nowrap table-responsive-stack"
-                                    id="tableOne">
+                                    <table class="table table-centered mb-0 table-nowrap">
                                         <thead class="thead-light">
                                         <tr>
                                             <th>SL</th>
@@ -197,69 +196,7 @@
             </div>
         </div> --}}
         @push('scripts')
-        <script>
-            $(document).ready(function() {
 
-
-        // inspired by http://jsfiddle.net/arunpjohny/564Lxosz/1/
-        $('.table-responsive-stack').each(function (i) {
-           var id = $(this).attr('id');
-           //alert(id);
-           $(this).find("th").each(function(i) {
-              $('#'+id + ' td:nth-child(' + (i + 1) + ')').prepend('<span class="table-responsive-stack-thead">'+             $(this).text() + ':</span> ');
-              $('.table-responsive-stack-thead').hide();
-
-           });
-
-
-
-        });
-
-
-
-
-
-        $( '.table-responsive-stack' ).each(function() {
-        var thCount = $(this).find("th").length;
-        var rowGrow = 100 / thCount + '%';
-        //console.log(rowGrow);
-        $(this).find("th, td").css('flex-basis', rowGrow);
-        });
-
-
-
-
-        function flexTable(){
-        if ($(window).width() < 768) {
-
-        $(".table-responsive-stack").each(function (i) {
-           $(this).find(".table-responsive-stack-thead").show();
-           $(this).find('thead').hide();
-        });
-
-
-        // window is less than 768px
-        } else {
-
-
-        $(".table-responsive-stack").each(function (i) {
-           $(this).find(".table-responsive-stack-thead").hide();
-           $(this).find('thead').show();
-        });
-
-
-
-        }
-        // flextable
-        }
-
-        flexTable();
-
-        window.onresize = function(event) {
-         flexTable();
-        };
-
-        });
         @endpush
 
 
