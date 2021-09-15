e78c5f46e089277f2af013516bdfea4512462c8f
Nazmul Huda <55596925+nazmul2858@users.noreply.github.com>
2021-09-15 23:56:14 +0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,23 @@
 
 namespace App\Providers;
 
+use App\Models\Backend\ProductInfo\Brand;
 use App\Models\Backend\ProductInfo\Category;
 use App\Models\Backend\ProductInfo\Product;
 use App\Models\Backend\ProductInfo\SubCategory;
 use App\Models\Backend\ProductInfo\SubSubCategory;
+use App\Models\Backend\Setting\BreakingNews;
 use App\Models\Backend\Setting\CompanyInfo;
 use App\Models\Backend\Setting\InvoiceSetting;
-use App\Models\Backend\Setting\BreakingNews;
 use App\Models\Backend\Setting\Language;
-use App\Models\Inventory\Currency;
-use App\Models\Setting\Slider;
-use App\Models\Backend\ProductInfo\Brand;
 use App\Models\District;
 use App\Models\FrontEnd\Order;
+use App\Models\Inventory\Currency;
+use App\Models\Setting\Slider;
+use App\Services\AddToCardService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use App\Services\AddToCardService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -56,7 +56,7 @@ class AppServiceProvider extends ServiceProvider
             $view->with('InvoiceSetting', InvoiceSetting::whereCreatedBy(Auth::id())->first());
             $view->with('currencySymbol', Currency::whereIsActive(1)->first());
             $view->with('cardBadge', AddToCardService::cardTotalProductAndAmount());
-            $view->with('BreakingNews', BreakingNews::get());
+            $view->with('BreakingNews', BreakingNews::whereIsActive(1)->get());
             $view->with('Districts', District::orderBy('name', 'asc')->get());
             $view->with('orders_count', Order::whereStatus('processing')->count());
         });
