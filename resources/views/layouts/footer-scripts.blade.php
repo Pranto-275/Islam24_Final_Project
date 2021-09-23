02c761f48a7304ff602f9096f00e4df7965e8420
md alamin <55638374+mdalmin291@users.noreply.github.com>
2021-09-23 18:07:47 +0600

    charts_class_add

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,19 +1,34 @@
 <!-- JAVASCRIPT -->
-<script src="{{ URL::asset('assets/libs/jquery/jquery.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/bootstrap/bootstrap.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/metismenu/metismenu.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/node-waves/node-waves.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/sweetalert2/sweetalert2.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/select2/select2.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/datatables/datatables.min.js')}}"></script>
-<script src="{{ URL::asset('assets/libs/moment/moment.min.js')}}"></script>
-<script src="{{URL::asset('assets/libs/tinymce/tinymce.min.js')}}"></script>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js"></script>
+<script src="{{ URL::asset('assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/bootstrap/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/metismenu/metismenu.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/node-waves/node-waves.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/select2/select2.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/datatables/datatables.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/moment/moment.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/tinymce/tinymce.min.js') }}"></script>
 
-<!-- App js -->
-<script src="{{ URL::asset('assets/js/app.min.js')}}"></script>
+{{-- charts script --}}
+<script src="{{ URL::asset('assets/libs/echarts/echarts.min.js') }}"></script>
+<!-- echarts init -->
+<script src="{{ URL::asset('assets/js/pages/echarts.init.js') }}"></script>
+
+<script src="{{ URL::asset('assets/js/app.js') }}"></script>
+
+<!-- tui charts plugins -->
+<script src="{{URL::asset('assets/libs/tui-chart/tui-chart.min.js')}}"></script>
+
+<!-- tui charts map -->
+<script src="{{ URL::asset('assets/libs/tui-chart/maps/usa.js') }}"></script>
+
+<!-- tui charts plugins -->
+<script src="{{ URL::asset('assets/js/pages/tui-charts.init.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
+
 
 @stack('scripts')
 
@@ -42,21 +57,19 @@
             text: message.text,
             type: 'success',
             confirmButtonColor: '#3b5de7'
-        }).then(function () {
+        }).then(function() {
             window.location = message.url;
         });
     });
 
     window.livewire.on('modal', message => {
-            $('#'+message).modal('toggle');
+        $('#' + message).modal('toggle');
     });
 
 
-    window.livewire.on('confirm', message  =>
-{
-    $('#'+message).modal('hide');
-});
-
+    window.livewire.on('confirm', message => {
+        $('#' + message).modal('hide');
+    });
 </script>
 
 
